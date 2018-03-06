c5d3e538209c3383b6e224252ca023e7012f37ce
DarthShoge <shogeluk@GMail.com>
2018-03-06 00:03:49 +0000

    Made changes to make deployable to AWS
    Made changes to make deployable to AWS

---
+++ arblab/__init__.py
@@ -0,0 +1,1 @@
+import arblab.arb_lab
--- arblab/utils.py
+++ arblab/utils.py
@@ -1,35 +1,7 @@
-from functools import reduce
-
-import ccxt
-import pandas as pd
 import math
+
 import numpy as np
-
-# def get_mkt_participants(pair):
-#     subclasses = set()
-#     work = [ccxt.Exchange]
-#     while work:
-#         parent = work.pop()
-#         for child in parent.__subclasses__():
-#             if child not in subclasses:
-#                 subclasses.add(child)
-#                 work.append(child)
-#     return subclasses
-
-
-usdt_exchanges = {
-    'binance': ccxt.binance(),
-    'hitbtc': ccxt.hitbtc(),
-    # 'huobi': ccxt.huobi(),
-    'poloniex': ccxt.poloniex(),
-    'bittrex': ccxt.bittrex(),
-    'gateio': ccxt.gateio(),
-    'kucoin': ccxt.kucoin(),
-    'tidex': ccxt.tidex(),
-    'cryptopia': ccxt.cryptopia(),
-    'okex': ccxt.okex()
-
-}
+import pandas as pd
 
 
 def get_clean_ticker(ex, pair):
@@ -47,9 +19,6 @@ def get_fee_structure(ex, pair):
         return {'ask': d['maker'], 'bid': d['taker']}
     except:
         return {}
-
-
-flatten = lambda lst: reduce(lambda l, i: l + flatten(i) if isinstance(i, (list, tuple)) else l + [i], lst, [])
 
 
 def get_exchange_pairs(arb_df):
@@ -145,38 +114,3 @@ def deduct_order_book_fees(order_books_dict, fee_df):
                     "vol": np.array([x[1] for x in order_book['bids']])}
         results[exchange] = {'ask': pd.DataFrame(ask_dict), 'bid': pd.DataFrame(bid_dict)}
     return results
-
-
-coin = 'LTC'
-pair = '%s/USDT' % coin
-
-coin_data = pd.DataFrame(
-    {key: get_clean_ticker(val, pair) for key, val in usdt_exchanges.items()}).transpose()
-
-# retrieve exchange-specific transaction fees
-fee_df = pd.DataFrame({key: get_fee_structure(ex, pair) for key, ex in usdt_exchanges.items()}).transpose()
-fee_df = fee_df.apply(abs)
-
-# bid/ask series without fee deduction
-ask_ser_nf = coin_data['ask'] * (1 + fee_df['ask'])
-bid_ser_nf = coin_data['bid'] * (1 - fee_df['bid'])
-
-# bid/ask series with fee deduction
-ask_ser = coin_data['ask'] * (1 + fee_df['ask'])
-bid_ser = coin_data['bid'] * (1 - fee_df['bid'])
-
-# matrix of arbitrage opportunities after deducting for exchange fees
-ones_df = pd.DataFrame(1, index=ask_ser.index, columns=bid_ser.index)
-arb_df = (ones_df.multiply(bid_ser, axis='index') / ask_ser) - 1
-relevant_arbs_df = arb_df[arb_df > 0]
-
-exchange_pairs = list(get_exchange_pairs(relevant_arbs_df))
-distinct_arb_exchanges = set(flatten(exchange_pairs))
-
-# get bid ask order books for each excahnge
-arb_ex_order_book_dict = {x: usdt_exchanges[x].fetch_order_book(pair) for x in distinct_arb_exchanges}
-# deduct exchange-specific fees from bid/ask prices in order books
-order_book_df_dict = deduct_order_book_fees(arb_ex_order_book_dict, fee_df)
-
-executable_amounts = pd.DataFrame(
-    [val for val in get_arb_result_series(exchange_pairs, ask_ser, bid_ser, order_book_df_dict)])
