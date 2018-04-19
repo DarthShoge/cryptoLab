344bb50a6ccf862603ebc8c3cac1f937343a18b6
DarthShoge <shogeluk@GMail.com>
2018-04-19 07:32:19 +0100

    ``

--- main.py
+++ main.py
@@ -3,8 +3,54 @@ import pandas as pd
 
 from arblab.arb_lab import find_arb_opportunities, get_mkt_participants
 
-pair = 'ETH/USD'
+pair = 'LTC/USDT'
 
-exchanges = get_mkt_participants(pair)
+# exchanges = get_mkt_participants(pair)
 
-v = find_arb_opportunities(exchanges, pair)
+exchanges = {'Binance': ccxt.binance(),
+             'HitBTC' : ccxt.hitbtc()}
+
+# exchanges = {'OKCoin USD': ccxt.okcoinusd(),
+#              'BitBay': ccxt.bitbay(),
+#              'Bitfinex': ccxt.bitfinex(),
+#              'Binance': ccxt.binance({'verbose': True}),
+#              'Bitstamp': ccxt.bitstamp(),
+#              'BTCTurk': ccxt.btcturk(),
+#              'C-CEX': ccxt.ccex(),
+#              'CoinMarketCap': ccxt.coinmarketcap(),
+#              'Coinsecure': ccxt.coinsecure(),
+#              'EXMO': ccxt.exmo(),
+#              'FYB-SE': ccxt.fybse(),
+#              'Gatecoin': ccxt.gatecoin(),
+#              'HitBtc': ccxt.hitbtc(),
+#              'GDAX': ccxt.gdax(),
+#              'Kraken': ccxt.kraken(),
+#              'LiveCoin': ccxt.livecoin(),
+#              'Lykke': ccxt.lykke(),
+#              'SouthXchange': ccxt.southxchange(),
+#              'Vaultoro': ccxt.vaultoro()}
+
+filtered_exchanges = { e:v for e,v in exchanges.items() if e not in ['CoinEx']}
+
+v = find_arb_opportunities(filtered_exchanges, pair)
+
+hit_btc : ccxt.Exchange = exchanges['HitBTC']
+hit_btc.apiKey = ''
+hit_btc.secret = ''
+hit_btc_balances = hit_btc.fetch_balance()
+
+binance = exchanges['Binance']
+binance.apiKey = ''
+binance.secret = ''
+binance_balances = binance.fetch_balance()
+
+# hl = hit_btc.create_limit_sell_order(pair, 0.1,150.00 )
+# ht = hit_btc.create_market_buy_order(pair, 0.1 )
+
+# hit_btc.payment_post_transfer_to_main ({
+#    'amount': 0.02,
+#    'currency_code': 'LTC',
+# })
+#
+# wh = hit_btc.withdraw('LTC',0.02, 'LRW15TRkMvy5B4T1KivpWfKWq5e87UtP5E')
+hit_btc.has()
