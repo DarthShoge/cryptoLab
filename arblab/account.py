5348a80dc9ddea074b54a0eff934b2e5a55d1162
Lukman Shoge <lshoge@millenniumglobal.com>
2018-04-23 16:54:46 +0100

    added account abstraction

---
+++ arblab/account.py
@@ -0,0 +1,71 @@
+from abc import ABCMeta
+
+import ccxt
+from ccxt import Exchange
+from numpy import nan
+
+
+class ExchangeAccount(metaclass=ABCMeta):
+
+    def __init__(self, exchange: Exchange, deposit_address, buy_coin, sell_coin):
+        self.sell_coin = sell_coin
+        self.buy_coin = buy_coin
+        self.deposit_address = deposit_address
+        self.exchange = exchange
+        if not exchange.has['withdraw']:
+            raise ccxt.ExchangeError('Exchange cannot withdraw')
+
+    def buy(self, pair, amount=nan, **kwargs):
+        return self.exchange.create_market_buy_order(pair, amount, params=kwargs)
+
+    def sell(self, pair, amount=nan, **kwargs):
+        return self.exchange.create_market_sell_order(pair, amount, params=kwargs)
+
+    def rebalance(self, coin, amount, to):
+        return self.exchange.withdraw(coin, amount, to)
+
+    def rebalance_sell(self, to):
+        balances = self.get_balances()
+        rebalance_value = balances[self.sell_coin] / 2
+        return self.rebalance(self.sell_coin, rebalance_value, to)
+
+    def rebalance_buy(self, to):
+        balances = self.get_balances()
+        rebalance_value = balances[self.buy_coin] / 2
+        return self.rebalance(self.buy_coin, rebalance_value, to)
+
+    def get_balances(self, **kwargs):
+        balances = {k: v for k, v in self.exchange.fetch_free_balance(kwargs).items() if v > 0}
+        return balances
+
+
+class HitBtcAccount(ExchangeAccount):
+    def __init__(self, deposit_address, buy_coin, sell_coin, exchange: ccxt.hitbtc = None):
+        ExchangeAccount.__init__(self, exchange if exchange is not None else ccxt.hitbtc(), deposit_address, buy_coin,
+                                 sell_coin)
+
+    def buy(self, pair, amount=nan, **kwargs):
+        balances = self.transfer_trade_to_main(self.buy_coin)
+        super(HitBtcAccount, self).buy_coin(pair, balances[self.buy_coin], kwargs)
+
+    def sell(self, pair, amount=nan, **kwargs):
+        balances = self.transfer_trade_to_main(self.buy_coin)
+        super(HitBtcAccount, self).sell_coin(pair, balances[self.buy_coin], kwargs)
+
+    def rebalance(self, coin, amount, to):
+        self.exchange.payment_post_transfer_to_main ({
+           'amount': amount,
+           'currency_code': coin,
+        })
+        return self.exchange.withdraw(coin, amount, to)
+
+    def transfer_trade_to_main(self, coin):
+        balances = self.get_balances(type='main')
+        if coin not in balances and balances[coin] < 0.0001:
+            self.exchange.payment_post_transfer_to_trading({
+                'amount': balances[coin],
+                'currency_code': coin,
+            })
+
+            balances = self.get_balances(type='trade')
+        return balances
